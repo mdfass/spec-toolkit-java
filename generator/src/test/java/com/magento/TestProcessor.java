a193cb23028bbe7046984e8d4f383a212257142f
David Mas <dmas@magento.com>
2019-05-02 16:26:11 +0200

    Migrate to junit 5

--- generator/src/test/java/com/magento/TestProcessor.java
+++ generator/src/test/java/com/magento/TestProcessor.java
@@ -1,9 +1,8 @@
 package com.magento;
 
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,19 +10,16 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class TestProcessor {
-  @Rule
-  public TemporaryFolder tempFolder = new TemporaryFolder(new File("target"));
 
   @Test
-  public void testEventParse() throws IOException {
-    File target = tempFolder.getRoot();
-    Processor.main(new String[] {"src/test/resources/definitions", target.getAbsolutePath()});
+  public void testEventParse(@TempDir File tempFolder) throws IOException {
+    Processor.main(new String[] {"src/test/resources/definitions", tempFolder.getAbsolutePath()});
 
     String[] events = new String[] {"CustomerShipmentDone.java", "LinePriceAdjusted.java",
         "LinesReceived.java", "LinesShipped.java", "Reported.java"};
 
-    Arrays.stream(events).forEach(e -> Assert.assertTrue(
-        Files.exists(Paths.get(target.getAbsolutePath() + "/com/fulfillment_management", e))));
+    Arrays.stream(events).forEach(e -> Assertions.assertTrue(
+        Files.exists(Paths.get(tempFolder.getAbsolutePath(), "com/fulfillment_management", e))));
   }
 
 }
